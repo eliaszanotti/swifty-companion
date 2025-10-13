9baa555bf50bead1291b49db8ec56617636dff8a
Elias ZANOTTI <zanotti.elias@gmail.com>
2025-10-13 10:35:40 +0200

    fix eslint

--- components/LoginForm.tsx
+++ components/LoginForm.tsx
@@ -8,36 +8,36 @@ interface LoginFormProps {
 }
 
 export default function LoginForm({ onLogin }: LoginFormProps) {
-	return (
-		<PaperView style={{ flex: 1, justifyContent: "center", padding: 16 }}>
-			<Card mode="elevated" style={styles.card}>
-				<Card.Content style={styles.cardContent}>
-					<Text variant="headlineMedium">Connectez-vous</Text>
-					<Text variant="bodyLarge">
+    return (
+        <PaperView style={{ flex: 1, justifyContent: "center", padding: 16 }}>
+            <Card mode="elevated" style={styles.card}>
+                <Card.Content style={styles.cardContent}>
+                    <Text variant="headlineMedium">Connectez-vous</Text>
+                    <Text variant="bodyLarge">
 						Connectez-vous avec votre compte 42
-					</Text>
-				</Card.Content>
-				<Card.Actions>
-					<Button mode="contained" onPress={onLogin} icon="login">
+                    </Text>
+                </Card.Content>
+                <Card.Actions>
+                    <Button mode="contained" onPress={onLogin} icon="login">
 						Se connecter avec 42
-					</Button>
-				</Card.Actions>
-			</Card>
-		</PaperView>
-	);
+                    </Button>
+                </Card.Actions>
+            </Card>
+        </PaperView>
+    );
 }
 
 const styles = StyleSheet.create({
-	content: {
-		flex: 1,
-		justifyContent: "center",
-	},
-	card: {
-		padding: 16,
-		gap: 32,
-	},
-	cardContent: {
-		marginBottom: 32,
-		gap: 16,
-	},
+    content: {
+        flex: 1,
+        justifyContent: "center",
+    },
+    card: {
+        padding: 16,
+        gap: 32,
+    },
+    cardContent: {
+        marginBottom: 32,
+        gap: 16,
+    },
 });
