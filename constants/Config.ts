09b221f9defdd6ac4eb231600bbc45f73e13a824
Elias ZANOTTI <zanotti.elias@gmail.com>
2025-10-27 12:01:28 +0100

    add env secured

--- constants/Config.ts
+++ constants/Config.ts
@@ -1,7 +1,7 @@
+import { API_CLIENT_ID, API_CLIENT_SECRET, API_BASE_URL } from '@env';
+
 export const Config = {
-	CLIENT_ID:
-		"u-s4t2ud-c64c6d4137bd483cbbce24d9a67cc435f1384b5d588d1d47cce8f7f066aa5ff6",
-	CLIENT_SECRET:
-		"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",
-	API_BASE_URL: "https://api.intra.42.fr",
+	CLIENT_ID: API_CLIENT_ID || "",
+	CLIENT_SECRET: API_CLIENT_SECRET || "",
+	API_BASE_URL: API_BASE_URL || "https://api.intra.42.fr",
 };
---
+++ types/env.d.ts
@@ -0,0 +1,5 @@
+declare module '@env' {
+  export const API_CLIENT_ID: string;
+  export const API_CLIENT_SECRET: string;
+  export const API_BASE_URL: string;
+}
